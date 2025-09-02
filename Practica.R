82ef7c12a3f81aef2180c8849f26890093c05ac9
J.IVAN LALANGUI PEÑA <7636340121@untrm.edu.pe>
2025-09-02 16:02:34 -0500

    clase practica

---
+++ Practica.R
@@ -0,0 +1,37 @@
+# Mi primera practica con R
+x <- "Hola"
+y <- "UNTRM"
+# f(x,y)=x + x^2 + 2xy
+# ayuda:F1 Fn + F1
+
+
+paste(x, y)
+
+paste(x,y, sep =" + ")
+
+paste(x, y)
+
+
+
+library(tidyverse)
+
+#pipe
+
+a1 <- "estudiante"
+
+
+a_1 <- "objet"]
+a.1 <- "b"
+
+
+notas <- c(16, 17, 19, 15)
+media <- mean(notas)
+media <- 
+  
+# ctrl +shift + m = %>%
+m <- c(16, 17, 19, 15) %>% 
+  mean()
+m
+  
+  
+  
